b64f846805ec2926737a8d710bf04f9eb4aad407
Aashvi Manakiwala <aashvi22@gmail.com>
2024-01-19 11:33:51 -0500

    moved index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './public/src/App';
 
 const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement);
